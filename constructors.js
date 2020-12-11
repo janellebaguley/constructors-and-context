9f298202b9c1e6d1ad63ef1b56ef558c24d84abf
janellebaguley <nellie.baguley@gmail.com>
2020-12-10 17:51:46 -0700

    contructors update

--- constructors.js
+++ constructors.js
@@ -107,7 +107,7 @@ prius.moveCar()
     this.savedPosts = savedPosts;
   }
 
-  User.prototype.addSavedPosts = function(id, title, rating){
+  User.prototype.addSavedPost = function(id, title, rating){
     this.id = id
     this.title = title;
     this.rating = rating;
@@ -126,8 +126,8 @@ prius.moveCar()
   // You will continue to use the constructor function you created in problem 5.
   // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
   
-  User.prototype.changePostRating = function(id, num){
+  User.prototype.changePostRating = function(id, rating){
     this.id = id;
-    this.newRating = newRating
+    this.rating = newRating
   }
   
